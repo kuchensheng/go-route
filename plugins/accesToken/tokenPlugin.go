89101dca00531607c4b09072c8e4fb84bf0153a6
kuchensheng <787211647@qq.com>
2022-02-12 01:15:55 +0800

    [add] 添加accessToken验证插件

---
+++ plugins/accesToken/tokenPlugin.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/rs/zerolog/log"
+	"isc-route-service/plugins"
+	"isc-route-service/utils"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var IscAccessTokenKey = "isc-access-token"
+
+var ac = &accessTokeConf{
+	Urls: []string{"/api/common"},
+}
+
+type accessTokeConf struct {
+	Urls []string `json:"urls"`
+}
+
+func init() {
+	//这里做初始化操作
+	pwd, _ := os.Getwd()
+	fp := filepath.Join(pwd, "license.json")
+	utils.OpenFileAndUnmarshal(fp, ac)
+}
+
+//Valid access token 验证
+func Valid(args ...interface{}) error {
+	req := args[0].(*http.Request)
+	uri := req.URL.Path
+	if !utils.IsInSlice(ac.Urls, uri) {
+		return nil
+	}
+	iscAccessToken := req.Header.Get(IscAccessTokenKey)
+	log.Info().Msgf("公共服务请求,isc-access-token=%s", iscAccessToken)
+	if iscAccessToken == "" {
+		return &plugins.PluginError{
+			StatusCode: "403",
+			Content: `{
+				"code": 403,
+				"message":"应用无权限访问"
+			}`,
+		}
+	} else {
+		//todo 需要连接redis
+	}
+	return nil
+
+}
