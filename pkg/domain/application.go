568952c67be2288b138ffc94fe7a9ad30ff85ccb
库陈胜 <xiongm@isyscore.com>
2022-02-23 17:24:04 +0800

    [update] 按照日志级别输出到不同的日志目录

--- pkg/domain/application.go
+++ pkg/domain/application.go
@@ -65,24 +65,7 @@ func init() {
 	if act != "" {
 		ApplicationConfig.readApplicationYaml(act)
 	}
-	level := ApplicationConfig.Server.Logging.Level
-	l := zerolog.InfoLevel
-	if level != "" {
-		l1, err := zerolog.ParseLevel(strings.ToLower(level))
-		if err != nil {
-			log.Warn().Msgf("日志设置异常，将使用默认级别 INFO")
-		} else {
-			l = l1
-		}
-		zerolog.SetGlobalLevel(l)
-	}
-	zerolog.CallerSkipFrameCount = 3
-	zerolog.TimeFieldFormat = time.RFC3339
-	out := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05.000"}
-	out.FormatLevel = func(i interface{}) string {
-		return strings.ToUpper(fmt.Sprintf(" [%s] [%-2s]", ApplicationConfig.Server.Name, i))
-	}
-	log.Logger = log.Logger.Output(out).With().Caller().Logger()
+	InitLog()
 }
 func ReadProfileYaml() {
 	if Profile != "" {
@@ -110,4 +93,74 @@ func (conf *AppServerConf) readApplicationYaml(act string) {
 			log.Fatal().Msgf("application.yml解析错误", err)
 		}
 	}
+}
+
+var loggerInfo zerolog.Logger
+var loggerDebug zerolog.Logger
+var loggerWarn zerolog.Logger
+var loggerError zerolog.Logger
+
+func InitLog() {
+	level := ApplicationConfig.Server.Logging.Level
+	l := zerolog.InfoLevel
+	if level != "" {
+		l1, err := zerolog.ParseLevel(strings.ToLower(level))
+		if err != nil {
+			log.Warn().Msgf("日志设置异常，将使用默认级别 INFO")
+		} else {
+			l = l1
+		}
+		zerolog.SetGlobalLevel(l)
+	}
+	zerolog.CallerSkipFrameCount = 3
+	zerolog.TimeFieldFormat = time.RFC3339
+	out := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02 15:04:05.000"}
+	out.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf(" [%s] [%-2s]", ApplicationConfig.Server.Name, i))
+	}
+	log.Logger = log.Logger.Output(out).With().Caller().Logger()
+	//添加hook
+	levelInfoHook := zerolog.HookFunc(func(e *zerolog.Event, l zerolog.Level, msg string) {
+		//levelName := l.String()
+		e1 := e
+
+		switch l {
+		case zerolog.DebugLevel:
+			e1 = loggerDebug.Debug()
+		case zerolog.InfoLevel:
+			e1 = loggerInfo.Info()
+		case zerolog.WarnLevel:
+			e1 = loggerWarn.Warn()
+		case zerolog.ErrorLevel:
+			e1 = loggerError.Error()
+		default:
+			//默认输出到stdError
+		}
+		e1.Msg(msg)
+	})
+	log.Hook(levelInfoHook)
+}
+
+func initLoggerFile(logDir string, fileName string) zerolog.Logger {
+	var l zerolog.Logger
+	logFile := filepath.Join(logDir, fileName)
+	if file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm); err == nil {
+		l = log.With().Logger()
+		l.Output(file)
+	}
+	return l
+}
+
+func init() {
+	// 创建日志目录
+	logDir := filepath.Join(".", "logs")
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		_ = os.Mkdir(logDir, os.ModePerm)
+	}
+	// 创建日志文件
+	loggerInfo = initLoggerFile(logDir, "app-info.log")
+	loggerDebug = initLoggerFile(logDir, "app-debug.log")
+	loggerWarn = initLoggerFile(logDir, "app-warn.log")
+	loggerError = initLoggerFile(logDir, "app-error.log")
+
 }
