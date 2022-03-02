37e7d81cadd6250c40f1284654f5a4e864ab8f80
库陈胜 <xiongm@isyscore.com>
2022-03-02 17:40:59 +0800

    [update]根据go语言规范调整插件代码结构以及优化编辑命令

--- docker/build.sh
+++ docker/build.sh
@@ -19,4 +19,9 @@ mv bin/s1 bin/server
 
 echo "压缩后的文件大小" + `size "bin/server"`
 echo "构建docker镜像,TAG =$TAG"
+
+#构建镜像前删除本地的data目录和logs目录
+rm -rf bin/logs
+rm -rf bin/data/plugins
+rm -rf bin/data/resources
 docker build -t 10.30.30.22:9080/isyscore/isc-route-service:"$TAG" . && docker push 10.30.30.22:9080/isyscore/isc-route-service:"$TAG"
--- plugins/compile.sh
+++ plugins/compile.sh
@@ -1,1 +1,18 @@
-go build -o ../docker/bin/plugins/$1/plugin.so -buildmode=plugin $1/plugin.go
+echo "删除../docker/bin/plugins/$1/plugin.so"
+rm -f ../docker/bin/plugins/"$1"/plugin.so
+echo "构建 $1/plugin.so"
+
+GOARCH=amd64 GOOS=linux go build -gcflags "all=-N -l" -ldflags "-s -w" -o ../docker/bin/plugins/"$1"/plugin.so -buildmode=plugin "$1"/plugin.go
+
+size() {
+  stat -c %s "$1" | tr -d '\n'
+}
+echo "构建完成，动态链接库大小" + `size "../docker/bin/plugins/"$1"/plugin.so"`
+echo "执行压缩命令"
+upx -o  ../docker/bin/plugins/"$1"/plugin1.so ../docker/bin/plugins/"$1"/plugin.so
+
+rm ../docker/bin/plugins/"$1"/plugin.so
+
+mv ../docker/bin/plugins/"$1"/plugin1.so ../docker/bin/plugins/"$1"/plugin.so
+echo "压缩完成，动态链接库大小" + `size "../docker/bin/plugins/"$1"/plugin.so"`
+echo "构建完毕"
